11d2c3efe686de0fbed6e14788c5d3ae2225c67e
Xuân Việt <xuanvietle208@gmail.com>
2026-09-20 10:47:08 +0700

    Add files via upload

--- Arduino/loadcell_adc_sketch.ino
+++ Arduino/loadcell_adc_sketch.ino
@@ -1,1 +1,17 @@
+#include "HX711.h"
 
+HX711 scale;
+const int dtPin = 3;
+const int sckPin = 2;
+
+void setup() {
+  Serial.begin(9600);
+  scale.begin(dtPin, sckPin);
+}
+void loop() {
+  if (scale.is_ready()) {
+    Serial.print(millis());
+    Serial.print(",");
+    Serial.println(scale.read()); // Đọc trực tiếp 24-bit thô
+  }
+}
